860075d31300735ea12e1ac78f91cbe7c232b345
elashavit <ela.shavit@gmail.com>
2025-12-09 23:11:48 +0200

    Update equation_utils.py

--- equation_utils.py
+++ equation_utils.py
@@ -1,4 +1,4 @@
-from sympy import Eq,symbols,solve
+from sympy import Eq, symbols, solve
 
 ELEMENTS = [
     'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
@@ -16,8 +16,6 @@ ELEMENTS = [
 ]
 
 def generate_equation_for_element(compounds, coefficients, element):
-    """Generates a symbolic equation for the given element from compounds and coefficients.  
-    Example: For H in reactants [{'H': 2}, {'O': 4, 'H': 1}], coefficients [a0, a1], returns 2*a0 + a1."""
     equation = 0
     for i, compound in enumerate(compounds):
         if element in compound:
@@ -26,14 +24,10 @@ def generate_equation_for_element(compounds, coefficients, element):
 
 
 def build_equations(reactant_atoms, product_atoms):
-    """Builds a list of symbolic equations for each element to balance a chemical reaction.  
-    Example: For H2 + O2 -> H2O, returns equations [2*a0 - 2*b0, a1 - b0]."""
-    ## coefficients ##
     reactant_coefficients = list(symbols(f'a0:{len(reactant_atoms)}'))
     product_coefficients = list(symbols(f'b0:{len(product_atoms)}')) 
-    product_coefficients = product_coefficients[:-1] + [1] # Ensure the last coefficient is 1
+    product_coefficients = product_coefficients[:-1] + [1]  # fix last product coefficient
 
-    ## equations ##
     equations = []
     for element in ELEMENTS:
         lhs = generate_equation_for_element(reactant_atoms, reactant_coefficients, element)
@@ -41,16 +35,15 @@ def build_equations(reactant_atoms, product_atoms):
         if lhs != 0 or rhs != 0:
             equations.append(Eq(lhs, rhs))
 
-    return equations, reactant_coefficients + product_coefficients[:-1]
+    
+    return equations, reactant_coefficients + product_coefficients
 
 
 def my_solve(equations, coefficients):
-    """Solves the system of equations for the coefficients of the reaction.  
-    Example: For equations [2*a0 - 2*b0, a1 - b0], returns [1.0, 1.0]."""
     solution = solve(equations, coefficients)
 
     if len(solution) == len(coefficients):
-        coefficient_values = list()
+        coefficient_values = []
         for coefficient in coefficients:
             coefficient_values.append(float(solution[coefficient]))
         return coefficient_values
