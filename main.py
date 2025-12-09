9033d79271aa9ae09d4ec83076800d258ecb3a28
elashavit <ela.shavit@gmail.com>
2025-12-09 23:09:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from equation_utils import ELEMENTS,generate_equation_for_element,build_equations,my_solve
-from string_utils import split_before_uppercases,split_at_digit,count_atoms_in_molecule,parse_chemical_reaction,count_atoms_in_reaction
-from sympy import Eq,symbols,solve
+from equation_utils import ELEMENTS, generate_equation_for_element, build_equations, my_solve
+from string_utils import split_before_uppercases, split_at_digit, count_atoms_in_molecule, parse_chemical_reaction, count_atoms_in_reaction
+from sympy import Eq, symbols, solve
 
 
-def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
+def balance_reaction(reaction):  # "Fe2O3 + H2 -> Fe + H2O"
 
-    # 1.parse reaction
-    reactants, products = parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
-    reactant_atoms = count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
+    # 1. parse reaction
+    reactants, products = parse_chemical_reaction(reaction)  # ["Fe2O3", "H2"], ["Fe", "H2O"]
+    reactant_atoms = count_atoms_in_reaction(reactants)      # [{"Fe":2, "O":3}, {"H":2}]
     product_atoms = count_atoms_in_reaction(products)
 
-    # 2.build equation and solve
+    # 2. build equation and solve
     equations, coefficients = build_equations(reactant_atoms, product_atoms)
-    coefficients = my_solve(equations, coefficients) + [1]
+    coefficients = my_solve(equations, coefficients)
 
-    return coefficients # [1/3, 1, 2/3, 1]
+    return coefficients
+
