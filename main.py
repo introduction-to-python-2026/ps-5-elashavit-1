fb1993fcc6f398828db3c447ca23dd7f79e958e0
elashavit <ela.shavit@gmail.com>
2025-12-09 23:32:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from equation_utils import ELEMENTS, generate_equation_for_element, build_equations, my_solve
-from string_utils import split_before_uppercases, split_at_digit, count_atoms_in_molecule, parse_chemical_reaction, count_atoms_in_reaction
+from equation_utils import ELEMENTS,generate_equation_for_element,build_equations,my_solve
+from string_utils import split_before_uppercases,split_at_digit,count_atoms_in_molecule,parse_chemical_reaction,count_atoms_in_reaction
+from sympy import Eq,symbols,solve
 
-def balance_reaction(reaction):  # "Fe2O3 + H2 -> Fe + H2O"
-    # 1. פירוק התגובה
-    reactants, products = parse_chemical_reaction(reaction)
-    reactant_atoms = count_atoms_in_reaction(reactants)
+
+def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
+
+    # 1.parse reaction
+    reactants, products = parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
+    reactant_atoms = count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
     product_atoms = count_atoms_in_reaction(products)
 
-    # 2. בניית משוואות ופתרונן
+    # 2.build equation and solve
     equations, coefficients = build_equations(reactant_atoms, product_atoms)
-    ordered_solution = my_solve(equations, coefficients)  # ← בלי + [1] ובלי float
+    coefficients = my_solve(equations, coefficients) + [1]
 
-    return ordered_solution
+    return coefficients # [1/3, 1, 2/3, 1]
