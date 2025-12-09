b09598af5a15222a281b0b377da78c861a007cff
elashavit <ela.shavit@gmail.com>
2025-12-09 23:38:26 +0200

    Update equation_utils.py

--- equation_utils.py
+++ equation_utils.py
@@ -1,44 +1,22 @@
-from sympy import Eq, symbols, solve
-from fractions import Fraction
-
-ELEMENTS = [
-    'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
-    'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca',
-    'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
-    'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr',
-    'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn',
-    'Sb', 'I', 'Te', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd',
-    'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb',
-    'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
-    'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th',
-    'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm',
-    'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds',
-    'Rg', 'Cn', 'Uut', 'Uuq', 'Uup', 'Uuh', 'Uus', 'Uuo'
-]
-
-def generate_equation_for_element(compounds, coefficients, element):
-    equation = 0
-    for i, compound in enumerate(compounds):
-        if element in compound:
-            equation += coefficients[i] * compound[element]
-    return equation
-
-def build_equations(reactant_atoms, product_atoms):
-    reactant_coefficients = list(symbols(f'a0:{len(reactant_atoms)}'))
-    product_coefficients = list(symbols(f'b0:{len(product_atoms)}'))
-    product_coefficients = product_coefficients[:-1] + [1]  # מקדם אחרון = 1
-
-    equations = []
-    for element in ELEMENTS:
-        lhs = generate_equation_for_element(reactant_atoms, reactant_coefficients, element)
-        rhs = generate_equation_for_element(product_atoms, product_coefficients, element)
-        if lhs != 0 or rhs != 0:
-            equations.append(Eq(lhs, rhs))
-
-    return equations, reactant_coefficients + product_coefficients[:-1]
+from equation_utils import ELEMENTS, generate_equation_for_element, build_equations
+from string_utils import parse_chemical_reaction, count_atoms_in_reaction
+from sympy import Eq, symbols, solve, Rational
 
 def my_solve(equations, coefficients):
     solution_list = solve(equations, coefficients)
     solution = solution_list[0]  # dict ראשון
-    return [Fraction(solution[c].p, solution[c].q) for c in coefficients]
+    # החזרה כרשימה של שברים מדויקים
+    return [Rational(solution[c].p, solution[c].q) for c in coefficients]
 
+def balance_reaction(reaction):
+    # 1. פירוק התגובה
+    reactants, products = parse_chemical_reaction(reaction)
+    reactant_atoms = count_atoms_in_reaction(reactants)
+    product_atoms = count_atoms_in_reaction(products)
+
+    # 2. בניית משוואות ופתרונן
+    equations, coefficients = build_equations(reactant_atoms, product_atoms)
+    ordered_solution = my_solve(equations, coefficients)
+
+    # 3. החזרת הרשימה הסופית של השברים
+    return ordered_solution
